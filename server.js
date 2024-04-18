9b7ff499cd8adff38d9f9fbd3ee6d7a7bd3429bd
Sarvar Xudoyberdiyev <sxweb20@gmail.com>
2024-04-18 14:27:48 +0500

    user cart

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from "express"
 import cors from "cors"
+import 'dotenv/config'
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
-import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
 
 // app config
 const app = express()
@@ -21,6 +22,7 @@ connectDB();
 app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
 app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
 
 app.get("/", (req, res) => {
   res.send("API worked")
