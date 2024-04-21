8c4efbccd0fd58849d9cc01a383013b37510785b
Sarvar Xudoyberdiyev <sxweb20@gmail.com>
2024-04-21 21:59:37 +0500

    order list added for admin

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -58,21 +58,57 @@ const placeOrder = async (req, res) => {
 }
 
 const verifyOrder = async (req, res) => {
-  const {orderId, success} = req.body;
+  const { orderId, success } = req.body;
   try {
-    if(success === "true") {
-      await orderModel.findByIdAndUpdate(orderId,{payment: true});
-      res.json({success:true, message: "Paid"});
-    }else {
+    if (success === "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Paid" });
+    } else {
       await orderModel.findByIdAndDelete(orderId);
-      res.json({success: false, message: "Not Paid"});
+      res.json({ success: false, message: "Not Paid" });
     }
 
 
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+}
+
+// user order for frontend
+
+const userOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({ userId: req.body.userId });
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+}
+
+// Listing orders for admin panel
+
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+}
+
+// api for updating order status
+
+const updateStatus = async (req, res) => {
+  try {
+    await orderModel.findByIdAndUpdate(req.body.orderId, {status: req.body.status});
+    res.json({success: true, message: "Status Updated"})
   } catch(error) {
     console.log(error);
     res.json({success: false, message: "Error"});
   }
 }
 
-export { placeOrder, verifyOrder }
+export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus }
