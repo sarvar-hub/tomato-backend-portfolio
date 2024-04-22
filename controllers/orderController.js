8c5f3ca99fd4b94479dfb99fe09a39d46c32e5e5
Sarvar Xudoyberdiyev <sxweb20@gmail.com>
2024-04-22 09:34:05 +0500

    added front url

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for frontend
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:3000";
+  const frontend_url = process.env.FRONT_URL;
 
   try {
     const newOrder = new orderModel({
